9df765bead11194a31c0eeae75bd1fbc3fca4abd
moliva <oliva.miguelh@gmail.com>
2025-01-13 01:12:47 -0300

    chore: enable status

--- src/identity.rs
+++ src/identity.rs
@@ -147,7 +147,7 @@ where
             }
             .boxed_local();
         }
-        if ["/login", "/auth", "/refresh", "/logout"].contains(&req.path()) {
+        if ["/status", "/login", "/auth", "/refresh", "/logout"].contains(&req.path()) {
             let srv = self.service.clone();
 
             return async move {
